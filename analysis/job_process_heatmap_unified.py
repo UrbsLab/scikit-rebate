82be7a5360b1d4b90f47fcd3bd0d8e8bf1323816
Kia Kazemi-Nia <kazemini@usc.edu>
2025-10-10 17:00:49 -0700

    Test bbox_inches=None

--- analysis/job_process_heatmap_unified.py
+++ analysis/job_process_heatmap_unified.py
@@ -177,22 +177,11 @@ def main():
             # Put "E" and "H" labels on the right side of the heatmaps in the last column
             if j == len(n_values) - 1:
                 # maybe add if clauses in the event that a dataset does not have EDM-1 or EDM-2
-                # ax_top.set_ylabel("E", rotation=0, labelpad=20, fontsize=18)
-                # ax_top.yaxis.set_label_position("right")
+                ax_top.set_ylabel("E", rotation=0, labelpad=20, fontsize=18)
+                ax_top.yaxis.set_label_position("right")
                 
-                # ax_bot.set_ylabel("H", rotation=0, labelpad=20, fontsize=18)
-                # ax_bot.yaxis.set_label_position("right")
-                ax_top.set_ylabel("E", rotation=0, fontsize=18)
-                ax_bot.set_ylabel("H", rotation=0, fontsize=18)
-
-                # compute x positions just outside axes but inside figure
-                bbox_top = ax_top.get_position()
-                bbox_bot = ax_bot.get_position()
-                label_x_top = min(bbox_top.x1 + 0.01, 0.99)  # ensure within figure
-                label_x_bot = min(bbox_bot.x1 + 0.01, 0.99)
-
-                ax_top.yaxis.set_label_coords(label_x_top, 0.5)
-                ax_bot.yaxis.set_label_coords(label_x_bot, 0.5)
+                ax_bot.set_ylabel("H", rotation=0, labelpad=20, fontsize=18)
+                ax_bot.yaxis.set_label_position("right")
 
     # # Add one colorbar on the far right
     # cbar_ax = fig.add_axes([1.02, 0.15, 0.02, 0.7])
@@ -334,7 +323,8 @@ def main():
     outdir = os.path.basename(os.path.normpath(args.basedir))
     parentdir = os.path.dirname(os.path.normpath(args.basedir))
     save_path = os.path.join(parentdir, outdir, args.prefix + "unified_heatmaps.pdf")
-    plt.savefig(save_path, format='pdf', bbox_inches='tight')
+    # plt.savefig(save_path, format='pdf', bbox_inches='tight')
+    plt.savefig(save_path, format='pdf', bbox_inches=None)
     plt.close()
     print(f"Unified heatmap saved to {save_path}")
 
