3ea9d0ec73889bf61593f211a3cca91cc319f2d9
Kia Kazemi-Nia <kazemini@usc.edu>
2025-12-12 09:41:29 -0800

    Added MuRelief10 and MuRelief100 to unified heatmap viz

--- analysis/job_process_heatmap_unified.py
+++ analysis/job_process_heatmap_unified.py
@@ -65,13 +65,14 @@ def main():
     # custom colormap & ordering
     custom_cmap = sns.color_palette('Oranges', n_colors=1000)[:800] + sns.color_palette('Blues', n_colors=1000)[800:]
 
-    # rba_order = [
-    #     'RandomShuffle','MutualInfo','ReliefF10','ReliefF100','SURF','SURFstar',
-    #     'MultiSURF','MultiSURFstar','SWRF','SWRFstar2','MultiSWRF','MultiSWRFstar','MultiSWRFDB','MultiSWRFDBstar'
-    # ]
     rba_order = [
-        'RandomShuffle', 'MutualInfo', 'MultiSWRFDB', 'MultiSWRFDBstar', 'MultiSWRFDBlinear', 'MultiSWRFDBlinear3SD', 'MultiSWRFDBlinearstar', 'MultiSWRFDBlinear3SDstar', 'MultiSWRFDBexponential', 'MultiSWRFDBexponential3SD', 'MultiSWRFDBexponentialstar', 'MultiSWRFDBexponential3SDstar'
+        'RandomShuffle','MutualInfo','ReliefF10','ReliefF100','SURF','SURFstar',
+        'MultiSURF','MultiSURFstar','SWRF','SWRFstar2','MultiSWRF','MultiSWRFstar','MultiSWRFDB','MultiSWRFDBstar',
+        'MuRelief10','MuRelief100'
     ]
+    # rba_order = [
+    #     'RandomShuffle', 'MutualInfo', 'MultiSWRFDB', 'MultiSWRFDBstar', 'MultiSWRFDBlinear', 'MultiSWRFDBlinear3SD', 'MultiSWRFDBlinearstar', 'MultiSWRFDBlinear3SDstar', 'MultiSWRFDBexponential', 'MultiSWRFDBexponential3SD', 'MultiSWRFDBexponentialstar', 'MultiSWRFDBexponential3SDstar'
+    # ]
     # rba_order = [
     #     'MultiSWRFDB', 'MultiSWRFDBstar'
     # ]
