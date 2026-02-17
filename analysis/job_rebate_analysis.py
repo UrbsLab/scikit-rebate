d7bf5c8ab2c6b32ecd723cd55110d59724cf622a
Kia Kazemi-Nia <kazemini@usc.edu>
2026-02-16 16:35:44 -0800

    Switching back to 16 cores per job for 10k feature/100k feature SURF/SURF* runs

--- analysis/job_rebate_analysis.py
+++ analysis/job_rebate_analysis.py
@@ -155,11 +155,11 @@ def process_relieff100(file_path):
     process_and_save_results(file_path, fs, "ReliefF100")
 
 def process_surf(file_path):
-    fs = SURF(n_jobs=1)
+    fs = SURF(n_jobs=16)
     process_and_save_results(file_path, fs, "SURF")
 
 def process_surfstar(file_path):
-    fs = SURFstar(n_jobs=1)
+    fs = SURFstar(n_jobs=16)
     process_and_save_results(file_path, fs, "SURFstar")
 
 def process_multisurf(file_path):
