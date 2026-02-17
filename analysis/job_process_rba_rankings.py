c53cac7e477d17e06ade82bd353c56da86a78b23
Kia Kazemi-Nia <kazemini@usc.edu>
2026-02-17 14:17:30 -0800

    Editing job_process_rba_rankings.py to handle 2wayEpiFeatures datasets

--- analysis/job_process_rba_rankings.py
+++ analysis/job_process_rba_rankings.py
@@ -41,7 +41,8 @@ def collect_rba_rankings(root_dir):
     for subdir, dirs, _ in os.walk(root_dir):
         subgroup_rankings_df = pd.DataFrame()
         # Only process Results folders that are within a_100 directories
-        if os.path.basename(subdir) == "Results" and "a_100" in subdir:
+        # if os.path.basename(subdir) == "Results" and "a_100" in subdir:
+        if os.path.basename(subdir) == "Results" and any(x in subdir for x in ["a_100", "a_1000", "a_10000", "a_100000"]):
             for rba in os.listdir(subdir):
                 # if "ABS" in rba or "Shuffle" in rba:
                 if "ABS" in rba:
