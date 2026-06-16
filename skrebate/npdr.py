8775ab95fb90ba363d07d027d48800778dda515e
kia-kn <kianw2014@gmail.com>
2026-06-15 17:05:51 -0700

    Finished creating distance matrix in NPDR

--- skrebate/npdr.py
+++ skrebate/npdr.py
@@ -6,11 +6,13 @@ from .multisurf import MultiSURF
 from .multisurfstar import MultiSURFstar
 from .baseswrf import SWRF, SWRFstar, MultiSWRF, MultiSWRFstar, MultiSWRFDB, MultiSWRFDBstar
 from .murelief import MuRelief
+import numpy as np
 import warnings
+import time
 
 class NPDR(ReliefF):
 
-    """Feature selection algorithm for continuous outcome data.
+    """Feature selection algorithm designed for continuous outcome data (also handles binary outcomes).
     Based on the NPDR algorithm as introduced in:
     T.T. Le, B.A. Dawkins, and B.A. McKinney
     Nearest-neighbor Projected-Distance Regression (NPDR) for detecting network interactions with adjustments for multiple tests and confounding
@@ -22,9 +24,9 @@ class NPDR(ReliefF):
         ----------
         regression_type: str (default: None)
             Type of regression to run on the outcome variable. Options are "linear" and "logistic". If input is not provided
-            this is automatically determined.
+            this is automatically determined based on the number of unique values in the outcome variable "y".
         relief_object: object (default: None)
-            A Relief-based algorithm (RBA) that has a _find_neighbors function. Used to identify neighbor instances for each target.
+            A core Relief-based algorithm (RBA) that has a _find_neighbors function. Used to identify neighbor instances for each target.
         padj_method: str (default: "fdr_bh")
             Method used to adjust p-values for multiple testing. 
         n_jobs: int (default: 1)
@@ -96,7 +98,7 @@ class NPDR(ReliefF):
         X: array-like {n_samples, n_features}
             Training instances to compute the feature importance scores from
         y: array-like {n_samples}
-            Training labels
+            Training outcomes
         covariates: None or array-like {n_samples, n_covariates}
             Covariates in per-attribute regression models. None or a matrix of covariate values.
 
@@ -128,12 +130,65 @@ class NPDR(ReliefF):
             elif len(self._label_list) <= 10:
                 self.regression_type = 'linear'
                 warnings.warn(
-                    "Detected between 3 and 10 unique y values. Multiclass data is not supported; applying linear regression.",
+                    "Detected between 3 and 10 unique y-values. Multiclass data is not supported; applying linear regression.",
                     UserWarning,
                 )
             else:
                 self.regression_type = 'linear'
 
         self._num_attributes = len(self._X[0])  # Number of predictors in training data
+
+        # Number of missing data values in predictor variable matrix.
+        self._missing_data_count = np.isnan(self._X).sum()
+
+        """Assign internal headers for the features (scikit-learn does not accept external headers from dataset):
+        The pre_normalize() function relies on the headers being ordered, e.g., X01, X02, etc.
+        If this is changed, then the sort in the pre_normalize() function needs to be adapted as well. """
+        xlen = len(self._X[0])
+        mxlen = len(str(xlen + 1))
+        self._headers = ['X{}'.format(str(i).zfill(mxlen)) for i in range(1, xlen + 1)]
+
+        # Determine data types for all features/attributes in training data (i.e. categorical or continuous)
+        C = D = False
+        # Examines each feature and applies categorical_threshold to determine variable type.
+        # Manually pre-setting categorical_features to None & categorical_threshold to 10 before application of _get_attribute_info
+        self.categorical_features = None
+        self.categorical_threshold = 10
+        self.attr = self._get_attribute_info()
+        for key in self.attr.keys():
+            if self.attr[key][0] == 'categorical':
+                D = True
+            if self.attr[key][0] == 'continuous':
+                C = True
+
+        # For downstream computational efficiency, determine if dataset is comprised of all categorical, all continuous, or a mix of categorical/continuous features.
+        if C and D:
+            self.data_type = 'mixed'
+        elif D and not C:
+            self.data_type = 'categorical'
+        elif C and not D:
+            self.data_type = 'continuous'
+        else:
+            raise ValueError('Invalid data type in data set.')
+        
+        # Compute the distance array between all data points ----------------------------------------------------------------
+        # For downstream efficiency, separate features in dataset by type (i.e. categorical/continuous); categorical = didx (discrete)
+        diffs, cidx, didx = self._dtype_array()
+        cdiffs = diffs[cidx]  # max/min continuous value difference for continuous features.
+
+        xc = self._X[:, cidx]  # Subset of continuous-valued feature data
+        xd = self._X[:, didx]  # Subset of categorical-valued feature data
+
+        self.distarray_has_nan = False
+        """ For efficiency, the distance array is computed more efficiently for data with no missing values.
+        This distance array will only be used to identify nearest neighbors. """
+        if self._missing_data_count > 0:
+            self._distance_array = self._distarray_missing(xc, xd, cdiffs)
+            
+            # if distance array has nan values, will use np.nanmean/np.nanstd downstream
+            if np.isnan(self._distance_array).any():
+                self.distarray_has_nan = True
+        else:
+            self._distance_array = self._distarray_no_missing(xc, xd)
 
         return self
