e3ea140c7550edb9b546ae28f6a3b42a73619e96
kia-kn <kianw2014@gmail.com>
2026-07-27 15:56:00 -0700

    Fixing small error with previous commit's TuRF refactoring

--- skrebate/turf.py
+++ skrebate/turf.py
@@ -13,7 +13,7 @@ class TURF(BaseEstimator):
                                         of features in the dataset.    
         :param num_scores_to_return:    Number of nonzero scores to return after training. Default = min(num_features, 10000)
         '''
-        if not self.check_is_int(num_scores_to_return) or num_scores_to_return <= 0 or (num_scores_to_return > 10000 and type(self.relief_object) not in (VLS,)):
+        if not self.check_is_int(num_scores_to_return) or num_scores_to_return <= 0 or (num_scores_to_return > 10000 and type(relief_object) not in (VLS,)):
             raise Exception('num_scores_to_return must be a positive integer <= 10,000 (when relief_object is a core RBA).\nIf relief_object is an instance of VLS, num_scores_to_return may exceed 10,000.')
 
         if not self.check_is_int(n_iterations) or n_iterations <= 0:
